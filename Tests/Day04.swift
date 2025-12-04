e2eb6bb06163c6f9b5723d4a72c27f4ccc17e5aa
Marco <50551133+marco906@users.noreply.github.com>
2025-12-04 18:14:58 +0100

    Update test data in Day04Tests

--- Tests/Day04.swift
+++ Tests/Day04.swift
@@ -7,40 +7,27 @@ import Testing
 struct Day04Tests {
   // Smoke test data provided in the challenge question
   let testData = """
-    ....XXMAS.
-    .SAMXMS...
-    ...S..A...
-    ..A.A.MS.X
-    XMASAMX.MM
-    X.....XA.A
-    S.S.S.S.SS
-    .A.A.A.A.A
-    ..M.M.M.MM
-    .X.X.XMASX
-    """
-  
-  let testData2 = """
-    .M.S......
-    ..A..MSMS.
-    .M.S.MAA..
-    ..A.ASMSM.
-    .M.S.M....
-    ..........
-    S.S.S.S.S.
-    .A.A.A.A..
-    M.M.M.M.M.
-    ..........
+    ..@@.@@@@.
+    @@@.@.@.@@
+    @@@@@.@.@@
+    @.@@@@..@.
+    @@.@@@@.@@
+    .@@@@@@@.@
+    .@.@.@.@@@
+    @.@@@.@@@@
+    .@@@@@@@@.
+    @.@.@@@.@.
     """
 
   @Test
   func testPart1() async throws {
     let challenge = Day04(data: testData)
-    #expect(String(describing: challenge.part1()) == "18")
+    #expect(String(describing: challenge.part1()) == "13")
   }
 
   @Test
   func testPart2() async throws {
-    let challenge = Day04(data: testData2)
-    #expect(String(describing: challenge.part2()) == "9")
+    let challenge = Day04(data: testData)
+    #expect(String(describing: challenge.part2()) == "43")
   }
 }
