a4e313b3cecb576a7d2a2f3f9a5e9e411485d208
Marco <50551133+marco906@users.noreply.github.com>
2025-12-04 18:14:08 +0100

    Update test cases for Day03

--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -1,24 +1,21 @@
-import Testing
-
-@testable import AdventOfCode
-
-// Make a copy of this file for every day to ensure the provided smoke tests
-// pass.
 struct Day03Tests {
   // Smoke test data provided in the challenge question
   let testData = """
-    xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+    987654321111111
+    811111111111119
+    234234234234278
+    818181911112111
     """
 
   @Test
   func testPart1() async throws {
     let challenge = Day03(data: testData)
-    #expect(String(describing: challenge.part1()) == "161")
+    #expect(String(describing: challenge.part1()) == "357")
   }
 
   @Test
   func testPart2() async throws {
     let challenge = Day03(data: testData)
-    #expect(String(describing: challenge.part2()) == "48")
+    #expect(String(describing: challenge.part2()) == "3121910778619")
   }
 }
