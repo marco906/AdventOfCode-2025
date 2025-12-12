a20af8709020edbc1bbd4f4fe90f17b24bd985ee
Marco <mar.wenzel@gmx.de>
2025-12-12 08:13:47 +0100

    solve day 12 part 1

--- Sources/Day12.swift
+++ Sources/Day12.swift
@@ -1,117 +1,59 @@
 struct Day12: AdventDay {
-  init(data: String) {
-    self.map = data.split(separator: "\n").map { $0.map { String($0) } }
-    numRows = map.count
-    numCols = map[0].count
+  var data: String
+  
+  struct Region {
+    let width: Int
+    let height: Int
+    let cells: Int
+    let pieces: Int
+    
+    var area: Int { width * height }
+    var boxes3x3: Int { (width / 3) * (height / 3) }
   }
   
-  var map: [[String]] = []
-  var numRows: Int
-  var numCols: Int
-
-  var directions: [[Int]] = [[0,1], [0, -1], [1, 0], [-1,0]]
-  var trailMemory = [Position:Int]()
-
-  struct Position: Hashable {
-    var x: Int
-    var y: Int
-  }
-
-  func isInBounds(_ position: Position) -> Bool {
-    guard position.x >= 0 && position.y >= 0 else { return false }
-    guard position.x < numCols && position.y < numRows else { return false }
-    return true
+  func parse() -> [Region] {
+    let blocks = data.components(separatedBy: "\n\n")
+    let cellCounts = blocks.filter { !$0.contains("x") }.map { $0.filter { $0 == "#" }.count }
+    
+    return data.split(separator: "\n")
+      .map { $0.trimmingCharacters(in: .whitespaces) }
+      .filter { $0.contains("x") }
+      .map { line in
+        let parts = line.split(separator: ":")
+        let dims = parts[0].split(separator: "x")
+        let pieceCount = parts[1].split(separator: " ").map { Int($0)! }
+        return Region(
+          width: Int(dims[0])!,
+          height: Int(dims[1])!,
+          cells: zip(pieceCount, cellCounts).map(*).reduce(0, +),
+          pieces: pieceCount.reduce(0, +)
+        )
+      }
   }
   
-  func value(_ position: Position) -> String {
-    return map[position.y][position.x]
-  }
-
-  func fence(_ type: String, pos: Position, area: inout Int, perimeter: inout Int, visited: inout [String:Set<Position>]) async {
-    if visited[type, default: []].contains(pos) {
-      return
-    }
-
-    guard isInBounds(pos) else {
-      visited[type, default: []].insert(pos)
-      perimeter += 1
-      return
-    }
-
-    let plant = value(pos)
-    if plant == type {
-      area += 1
-      visited[type, default: []].insert(pos)
-    } else {
-      perimeter += 1
-      return
-    }
-
-    for dir in directions {
-      let newPos = Position(x: pos.x + dir[0], y: pos.y + dir[1])
-      await fence(type, pos: newPos, area: &area, perimeter: &perimeter, visited: &visited)
-    }
-  }
-
-  func fence(_ type: String, pos: Position, area: inout Int, edges: inout Set<Position>, visited: inout [String:Set<Position>]) async {
-    guard isInBounds(pos) else {
-      visited[type, default: []].insert(pos)
-      edges.insert(pos)
-      return
-    }
-
-    if visited[type, default: []].contains(pos) {
-      edges.remove(pos)
-      return
-    }
-
-    let plant = value(pos)
-    if plant == type {
-      area += 1
-      edges.remove(pos)
-      visited[type, default: []].insert(pos)
-    } else {
-      edges.insert(pos)
-      return
-    }
-
-    for dir in directions {
-      let newPos = Position(x: pos.x + dir[0], y: pos.y + dir[1])
-      await fence(type, pos: newPos, area: &area, edges: &edges, visited: &visited)
-    }
+  func canFit(_ region: Region) -> Bool? {
+    if region.cells > region.area { return false }
+    // Since all parts have a 3x3 bounding box, they always fit if area is large enough
+    if region.pieces <= region.boxes3x3 { return true }
+    return nil
   }
 
   func part1() async -> Any {
-    var cost = 0
-    var visited = [String:Set<Position>]()
-
-    for y in 0..<map.count {
-      for x in 0..<map[y].count {
-        let type = map[y][x]
-        var area = 0
-        var perimeter = 0
-        await fence(type, pos: Position(x: x, y: y), area: &area, perimeter: &perimeter, visited: &visited)
-        cost += area * perimeter
+    var count = 0
+    var uncertain = 0
+    for region in parse() {
+      if let fits = canFit(region) {
+        if fits { count += 1 }
+      } else {
+        uncertain += 1
       }
     }
-
-    return cost
+    
+    if uncertain > 0 {
+      return "Sorry, optimistic approach doesn't work: \(uncertain) regions need backtracking"
+    }
+    return count
   }
   
-  func part2() async -> Any {
-    var cost = 0
-    var visited = [String:Set<Position>]()
-
-    for y in 0..<map.count {
-      for x in 0..<map[y].count {
-        let type = map[y][x]
-        var area = 0
-        var edges = Set<Position>()
-        await fence(type, pos: Position(x: x, y: y), area: &area, edges: &edges, visited: &visited)
-        cost += area * edges.count
-      }
-    }
-
-    return cost
-  }
+  func part2() async -> Any { 0 }
 }
--- Tests/Day12.swift
+++ Tests/Day12.swift
@@ -2,32 +2,53 @@ import Testing
 
 @testable import AdventOfCode
 
-// Make a copy of this file for every day to ensure the provided smoke tests
-// pass.
-// struct Day12Tests {
-//   // Smoke test data provided in the challenge question
-//   let testData = """
-//     RRRRIICCFF
-//     RRRRIICCCF
-//     VVRRRCCFFF
-//     VVRCCCJFFF
-//     VVVVCJJCFE
-//     VVIVCCJJEE
-//     VVIIICJJEE
-//     MIIIIIJJEE
-//     MIIISIJEEE
-//     MMMISSJEEE
-//     """
+struct Day12Tests {
+  // Smoke test data provided in the challenge question
+  let testData = """
+    0:
+    ###
+    ##.
+    ##.
 
-//   @Test
-//   func testPart1() async throws {
-//     let challenge = Day12(data: testData)
-//     await #expect(String(describing: challenge.part1()) == "1930")
-//   }
+    1:
+    ###
+    ##.
+    .##
 
-//   @Test
-//   func testPart2() async throws {
-//     let challenge = Day12(data: testData)
-//     await #expect(String(describing: challenge.part2()) == "1206")
-//   }
-// }
+    2:
+    .##
+    ###
+    ##.
+
+    3:
+    ##.
+    ###
+    ##.
+
+    4:
+    ###
+    #..
+    ###
+
+    5:
+    ###
+    .#.
+    ###
+
+    4x4: 0 0 0 0 2 0
+    12x5: 1 0 1 0 2 2
+    12x5: 1 0 1 0 3 2
+    """
+
+  @Test
+  func testPart1() async throws {
+    let challenge = Day12(data: testData)
+    await #expect(String(describing: challenge.part1()) == "2")
+  }
+
+  @Test
+  func testPart2() async throws {
+    let challenge = Day12(data: testData)
+    await #expect(String(describing: challenge.part2()) == "1206")
+  }
+}
